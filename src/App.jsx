e2976ad7acfee464fb8df151c7faf6a70c2d6fbc
Mr. Mendez <mrmendez@amvestor.com>
2022-10-28 23:27:55 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import Footer from "./lib/Footer";
 import Clock from "./lib/shared/Clock";
 import NavBar from "./lib/NavBar";
+import Semaforo from "./lib/shared/StreetLight";
 
 function App() {
   return (
     <>
       {/* <NavBar /> */}
-      <main className="w-full">
+      <main className="mx-auto my-auto">
         <Clock />
+        {/* <Semaforo /> */}
       </main>
       {/* <Footer /> */}
     </>
