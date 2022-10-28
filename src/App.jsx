b1b376ee1cdabdbead03a92db98678531ff36cba
Mr. Mendez <mrmendez@amvestor.com>
2022-10-28 23:04:36 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
-import { Card } from "./lib/shared/Card";
 import Footer from "./lib/Footer";
-import Header from "./lib/Header";
-import Services from "./lib/sections/Services";
-import Clock from "./lib/sections/Clock";
+import Clock from "./lib/shared/Clock";
 import NavBar from "./lib/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <main className="w-full">
         <Clock />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/lib/shared/Clock.jsx
+++ src/lib/shared/Clock.jsx
@@ -5,11 +5,11 @@ export default function Clock() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setClock((prevClock) => prevClock + 1);
+      setClock(Clock + 1);
     }, 1000);
-
     return () => clearInterval(interval);
-  }, []);
+  }, [Clock]);
+
   return (
     <section className="mx-auto h-36 w-36 bg-slate-300 p-8 shadow-md transition-all duration-500 hover:scale-[1.01] hover:shadow-2xl">
       <h1 className="text-center align-middle text-7xl">{Clock}</h1>
